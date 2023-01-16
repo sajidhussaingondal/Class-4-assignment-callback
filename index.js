f856e1963be1dc6ebb0076c8febfa3ba0011d7b9
sajidhussaingondal <sajidgondal@gmail.com>
2023-01-16 21:11:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+/*
+Assignment:
+
+Make A Function Using Callbacks :
+Doing These Things:
+1-Ready For Scholl At 8 Am
+2-Sit On School Bus
+3-Attend Classes
+4-Return To HOME
+
+Output should be like this
+Ready for school at 8am
+Brushed teeth and had breakfast
+Sitting on school bus
+Listened to music on the bus ride
+Attending classes
+Learned about math and science
+Returning home
+Ate dinner and did homework
+*/
