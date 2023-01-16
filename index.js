bed3d4d3c817caf8b26f4c981a5c4843c5ee633e
sajidhussaingondal <sajidgondal@gmail.com>
2023-01-16 23:39:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -18,3 +18,67 @@ Learned about math and science
 Returning home
 Ate dinner and did homework
 */
+
+function sayHello() {
+  let you = prompt("What's your name? ");
+  console.log(`Hello, ${you}  !`);
+}
+sayHello();
+
+let varContainingFunction = function () {
+  let varInFunction = "I'm in a function.";
+  console.log("hi there!", varInFunction);
+};
+varContainingFunction();
+
+/*
+        See if you can write a function for yourself. We want to write a function
+that adds two numbers.
+1. Create a function that takes two parameters, adds the parameters
+together, and returns the result.
+2. Set up two different variables with two different values.
+3. Use your function on the two variables, and output the result using
+console.log .
+4. Create a second call to the function using two more numbers as
+arguments sent to the function.
+*/
+
+function doAddition(a, b) {
+  let c = a + b;
+  console.log(c);
+}
+doAddition(2, 5);
+
+let additionFunction = function (g, h) {
+  let f = g + h;
+  console.log(f);
+};
+additionFunction(4, 6);
+
+let multiplicationFunction = function (n1, n2) {
+  let n3 = n1 * n2;
+  console.log(n3);
+};
+multiplicationFunction(5, 6);
+
+// console.log (Math.floor (Math.random()*10));
+
+let newArray = ["Great", "Good", "Excellent", "Humble"];
+function provideName() {
+  let yourName = prompt("What is your name? ");
+  let arrSelection = Math.floor(Math.random() * newArray.length);
+  console.log(newArray[arrSelection] + " " + yourName);
+}
+provideName();
+
+const val1 = 10;
+const val2 = 5;
+let operat = "-";
+function cal(a, b, op) {
+  if (op == "-") {
+    console.log(a - b);
+  } else {
+    console.log(a + b);
+  }
+}
+cal(val1, val2, operat);
